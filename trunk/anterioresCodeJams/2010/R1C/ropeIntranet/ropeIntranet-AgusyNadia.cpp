18f81cc65c73936fc0de18503ed3aca4d2e479ba
heredianadia@gmail.com <heredianadia@gmail.com@132d8716-348d-89a7-68be-e1c8d3029e1a>
2011-05-05 03:21:10 +0000

    Rope intranet que anda

---
+++ trunk/anterioresCodeJams/2010/R1C/ropeIntranet/ropeIntranet-AgusyNadia.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<cstdio>
+#include<fstream>
+#include<cstdlib>
+#include<vector>
+#include<list>
+#include<queue>
+#include<algorithm>
+
+#define forn(i,n) 	 for(int i=0; i<n; i++)
+#define fornd(i,n) 	 for(int i=n; i>0; i--)
+#define fornx(i,x,n) for(int i=x; i<n; i++)
+#define forall(it,X) for(typeof((X).begin()) it = (X).begin(); it != (X).end(); it++)
+
+using namespace std;
+
+bool comp(pair<int, int> a, pair<int, int> b) 
+{
+    return a.first < b.first;
+}
+
+int main(int argc, char** argv)
+{
+	ifstream entrada("A-large.in");
+	ofstream salida("A-large.out");
+	
+	int casos;
+	entrada >> casos;
+	
+	forn(i, casos)
+	{
+		int n;		
+		entrada >> n;
+		
+		vector<pair<int, int> > ventanas;
+		
+		forn(j, n)
+		{
+			int a, b;
+			entrada >> a >> b;
+			ventanas.push_back(make_pair(a, b));
+		}		
+			
+		sort(ventanas.begin(), ventanas.end(), comp);
+		//~ forn(j, n)
+			//~ cout << ventanas[j].first << " " << ventanas[j].second << endl;
+		//~ cout << endl;
+		
+		long long int res = 0;		
+		
+		forn(j, n)
+		{
+			fornx(k, j+1, n)
+			{
+				if (ventanas[k].second < ventanas[j].second)
+					res++;
+			}
+		}
+		
+		salida << "Case #" << i+1 << ": " << res << endl;
+	}
+	
+	return 0;	
+}
